17db83f8bf2009f2896a630a051817eae7e25bd3
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-05-30 20:08:39 +0000

    Estructura del proyecto

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+let quien = ["EL PERRO", "EL ABUELO", "EL MECANICO"];
+let verbo = ["ORINO", "MORDIO", "DAÑO"];
+let cuando = ["HOY", "AYER", "HACE UNAS HORAS"];
+let que = ["LA TAREA", "EL CAUCHO", "EL CARRO"];
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const elemento = document.querySelector("#name");
+  let randomQuien = quien[Math.floor(Math.random() * quien.length)];
+  let randomVerbo = verbo[Math.floor(Math.random() * verbo.length)];
+  let randomCuando = cuando[Math.floor(Math.random() * cuando.length)];
+  let randomQue = que[Math.floor(Math.random() * que.length)];
+
+  elemento.innerText = `${randomQuien} ${randomVerbo} ${randomCuando} ${randomQue}`;
+  console.log(quien[Math.floor(Math.random() * quien.length)]);
 };
